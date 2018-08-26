b3eaa71fa51f5d90834fbbf193496e219b8fca56
HYP <1670787053@qq.com>
2018-08-26 15:21:24 +0800

    2018.8.26
    完善，增加LogicUtils和UUIDUtils

---
+++ src/main/java/com/jarvis/base/util/UUIDUtils.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2013-2017 Tencent Cloud. All Rights Reserved.
+ * Tencent PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ */
+package com.jarvis.base.util;
+
+import java.util.UUID;
+
+/**
+ * UUID的工具类
+ * 
+ * @author Tencent Cloud
+ * @author CETC55
+ * @since 2017/7/3
+ * @version v1.0
+ */
+public class UUIDUtils {
+
+	public static String createUUID() {
+		return UUID.randomUUID().toString();
+	}
+
+	/**
+	 * 获取UUID qudia
+	 * @return
+	 * @author zhaodun
+	 */
+	public static String getUUID() {
+		return createUUID().replace("-", "");
+	}
+}
